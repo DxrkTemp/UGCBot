04e62d991bf510f01f9ef0f2f691657763c4e39c
DxrkTemp <jjhombid@mcm.edu.ph>
2026-06-17 09:14:47 +0800

    Update db.js

--- db.js
+++ db.js
@@ -15,13 +15,11 @@ const UserSchema = new mongoose.Schema({
 const FashionRelease = require("./models/FashionRelease");
 const ScavengerHunt = require("./models/ScavengerHunt");
 const PaidLimited = require("./models/PaidLimited");
-const Affiliate = require("./models/Affiliate");
 
 module.exports = {
     connectDB,
     User: mongoose.model("User", UserSchema),
     FashionRelease,
     ScavengerHunt,
-    PaidLimited,
-    Affiliate
+    PaidLimited
 };
