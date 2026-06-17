99e0ee672d5d592bb9a0166f5b7dad72df85df0a
DxrkTemp <jjhombid@mcm.edu.ph>
2026-06-17 08:22:12 +0800

    Update time.js

--- utils/time.js
+++ utils/time.js
@@ -1,10 +1,17 @@
 const { DateTime } = require("luxon");
 
 function estToUTC(dateString) {
-    return DateTime
-        .fromISO(dateString, { zone: "America/New_York" })
-        .toUTC()
-        .toJSDate();
+    const dt = DateTime.fromFormat(
+        dateString,
+        "yyyy-MM-dd HH:mm",
+        { zone: "America/New_York" }
+    );
+
+    if (!dt.isValid) {
+        return null;
+    }
+
+    return dt.toUTC().toJSDate();
 }
 
 module.exports = { estToUTC };
