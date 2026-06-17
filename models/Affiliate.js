ec7e8f6bb33a290573616c8c87b3c1bdf5a172b2
DxrkTemp <jjhombid@mcm.edu.ph>
2026-06-17 09:03:45 +0800

    Update Affiliate.js

--- models/Affiliate.js
+++ models/Affiliate.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
 
+const AffiliateSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    affiliateName: { type: String, required: true },
+    releaseDate: { type: Date, required: true },
+    announced: { type: Boolean, default: false },
+    active: { type: Boolean, default: true }
+});
+
+module.exports = mongoose.model("Affiliate", AffiliateSchema);
