7143f201dd84b5def2e44b3b54b545d5229f6c5b
DxrkTemp <jjhombid@mcm.edu.ph>
2026-06-17 11:54:17 +0800

    Update FashionRelease.js

--- models/FashionRelease.js
+++ models/FashionRelease.js
@@ -2,15 +2,11 @@ const mongoose = require("mongoose");
 
 const FashionReleaseSchema = new mongoose.Schema({
     title: { type: String, required: true },
-
     releaseDate: { type: Date, required: true },
-
     format: { type: String, required: true },
-
     previewUrl: { type: String, default: "" },
-
+    bannerUrl: { type: String, default: "" },
     announced: { type: Boolean, default: false },
-
     active: { type: Boolean, default: true }
 });
 
