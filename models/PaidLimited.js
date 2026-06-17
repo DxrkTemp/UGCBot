bc4a8d624509df1114a3b48412aa59e1b16d0942
DxrkTemp <jjhombid@mcm.edu.ph>
2026-06-17 12:53:03 +0800

    Update PaidLimited.js

--- models/PaidLimited.js
+++ models/PaidLimited.js
@@ -2,8 +2,13 @@ const mongoose = require("mongoose");
 
 const PaidLimitedSchema = new mongoose.Schema({
     itemName: { type: String, required: true },
+    price: { type: String, default: "Not Set" },       
+    copies: { type: Number, default: 0 },             
+
     releaseDate: { type: Date, required: true },
+
     bannerUrl: { type: String, default: "" },
+
     announced: { type: Boolean, default: false },
     removed: { type: Boolean, default: false },
     active: { type: Boolean, default: true }
